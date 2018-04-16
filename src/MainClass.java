84ff09b3481773e77d6efadb861bd5c04a870a9c
karthikgitlink <38420009+karthikgitlink@users.noreply.github.com>
2018-04-16 14:04:26 +0530

    Added from browser
    Added from browser to test

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,6 +4,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Main Class Updated New Branch Update only in branch");
+		System.out.println("Added from browser");
 	}
 
 }
