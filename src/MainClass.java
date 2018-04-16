4396591c545649f369b62e4c889f16fedbbe775c
10648508 <10648508@172.28.102.160>
2018-04-16 15:19:09 +0530

    Vasu sir commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("Main Class Updated New Branch Update only in branch");
 		System.out.println("Added from browser");
+		System.out.println("Vasu Sir");
 	}
 
 }
